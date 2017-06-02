202c16f21d59cffce6846b58bdab4fa41e3422ab
luolp <llp_liulangcat@qq.com>
2017-06-02 16:53:01 +0800

    2017-06-02

--- Public/js/new-swiper.js
+++ Public/js/new-swiper.js
@@ -28,8 +28,8 @@ var indexSwiper = new Swiper('.index-swiper-container', {
     pagination : '.swiper-pagination',  //分页器
     paginationClickable :true,      //可点击
 });
-var lvyouwendaSwiper = new Swiper('.lvyouwenda-container', {
-    autoplay: 2000,//可选选项，自动滑动
+var indexSwiper2 = new Swiper('.index-container-1', {
+    autoplay: 3000,//可选选项，自动滑动
     autoplayDisableOnInteraction : false,//用户操作轮播图后，false为继续滑动 -->
     speed:600,     //开始到结束时间300毫秒
     loop : true,   //首尾连接循环轮播图
